f91f73da2f6e61d983236c522cb30b407b06953a
brymes83 <106006052+brymes83@users.noreply.github.com>
2022-05-24 19:20:06 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-region = "us-west-2"
-access_key = "AKIAV5TZWNDTACJRCZ7H"
-secret_key = "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"
-}
-
 resource "aws_instance" "ec2" {
   ami           = "ami-0ca285d4c2cda3300"
   instance_type = "t2.micro"
